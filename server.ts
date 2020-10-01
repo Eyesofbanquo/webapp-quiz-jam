5dc872e7e635debafbcf4430a46b1bcf4407f57d
Markim Shaw <ms79723@gmail.com>
2020-10-01 13:01:57 -0500

    Add initial files

---
+++ server.ts
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get("/api/welcome", (req, res) => {
+  res.json([
+    {
+      haha: "hehe",
+    },
+  ]);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+app.listen(port, () => console.log("Running..."));
