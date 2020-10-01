5a2adece509267078eb13ccbf0a2567250b5377c
Markim Shaw <ms79723@gmail.com>
2020-10-01 13:46:25 -0500

    Add port

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
