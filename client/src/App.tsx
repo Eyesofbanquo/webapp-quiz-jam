92b45d3e8642373914d4386bb6b12ce7b5010398
Markim Shaw <ms79723@gmail.com>
2020-10-02 01:01:23 -0500

    Add react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -25,7 +26,33 @@ const App = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  return <div>{data?.haha}</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <a href="/lol">League of Legends</a>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path="/">
+          <div>{data?.haha}</div>
+        </Route>
+        <Route exact path="/about">
+          <h1>About page</h1>
+        </Route>
+        <Route exact path="/lol">
+          <h1>Morde is trash</h1>
+        </Route>
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
