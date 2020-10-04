4ca346ef8a991a1e0105b353e4563424ee353d87
Markim Shaw <ms79723@gmail.com>
2020-10-04 04:30:20 -0500

    Create database class
    Create a database class for handling schema versions

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as Realm from "realm";
+import { Database } from "./database";
 
 interface AnswerResponse {
   category: string;
@@ -11,30 +12,29 @@ interface AnswerResponse {
 
 interface MultipleChoice {
   correct_answer: string;
-  incorrect_answer: string[];
+  incorrect_answers: string[];
 }
 
 interface TFChoice {
   correct_answer: "True" | "False";
-  incorrect_answer: ["True"] | ["False"];
+  incorrect_answers: ["True"] | ["False"];
 }
-
-const choice: TFChoice = {
-  correct_answer: "True",
-  incorrect_answer: ["False"],
-};
 
 const MultipleChoiceSchema = {
   name: "MultipleChoice",
+  primaryKey: "question",
   properties: {
+    id: "int",
     category: "string",
     type: "string",
     difficulty: "string",
     question: "string",
     correct_answer: "string",
-    incorrect_answer: "string[]",
+    incorrect_answers: "string[]",
   },
 };
+
+const db = new Database();
 
 export const API = express.Router();
 API.use(bodyParser.json());
@@ -55,7 +55,47 @@ API.get("/open_reference_tf", (request, response) => {
 type AnsweredResponse = AnswerResponse | undefined;
 
 API.post("/multiple", (request, response) => {
-  const receivedBody = request.body;
+  const receivedBody = request.body as AnswerResponse & MultipleChoice;
 
-  response.send(request.body);
+  const isValid =
+    receivedBody.category !== undefined &&
+    receivedBody.correct_answer !== undefined &&
+    receivedBody.difficulty !== undefined &&
+    receivedBody.incorrect_answers !== undefined &&
+    receivedBody.question !== undefined &&
+    receivedBody.correct_answer !== undefined;
+
+  if (isValid === false) {
+    response.statusCode = 400;
+    response.json({
+      error: "Incorrect format",
+    });
+  } else {
+    db.realm(MultipleChoiceSchema).then((realm) => {
+      realm.write(() => {
+        const newQuestion = realm.create("MultipleChoice", {
+          id: 0,
+          category: receivedBody.category,
+          type: receivedBody.type,
+          difficulty: receivedBody.difficulty,
+          question: receivedBody.question,
+          correct_answer: receivedBody.correct_answer,
+          incorrect_answers: receivedBody.incorrect_answers,
+        });
+      });
+
+      realm.close();
+    });
+
+    response.statusCode = 204;
+    response.send(request.body);
+  }
+});
+
+API.get("/multiple", async (request, response) => {
+  await db.realm(MultipleChoiceSchema).then((realm) => {
+    const allObjects: any = realm.objects("MultipleChoice");
+    response.send(allObjects);
+    realm.close();
+  });
 });
---
+++ src/database/database.ts
@@ -0,0 +1,12 @@
+import * as Realm from "realm";
+
+export class Database {
+  public static readonly SCHEMA_VERSION = 1;
+
+  realm(schema): Promise<Realm> {
+    return Realm.open({
+      schema: [schema],
+      schemaVersion: Database.SCHEMA_VERSION,
+    });
+  }
+}
