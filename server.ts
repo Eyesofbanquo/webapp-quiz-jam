75cb0a8bae97d8353d238ef78a45578c77d99f7b
Markim Shaw <ms79723@gmail.com>
2020-10-04 23:10:17 -0500

    Add ssl redirect fix

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as Realm from "realm";
+import sslRedirect from "heroku-ssl-redirect";
 import { API } from "./src";
 
-const ssl = require("heroku-ssl-redirect");
 const path = require("path");
 const cors = require("cors");
 
@@ -11,7 +11,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(ssl);
+app.use(sslRedirect());
 app.use("/api", API);
 app.use(cors());
 app.use(bodyParser.json());
