7819feac8498cf1f4bdf70f2cef7fd0bdd465b13
Eyesofbanquo <djsalinger67@live.com>
2020-10-04 23:02:06 -0500

    Add SSL redirect support (#10)
    Co-authored-by: Markim Shaw <ms79723@gmail.com>

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as Realm from "realm";
 import { API } from "./src";
+
+const ssl = require("heroku-ssl-redirect");
 const path = require("path");
 const cors = require("cors");
 
@@ -9,6 +11,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(ssl);
 app.use("/api", API);
 app.use(cors());
 app.use(bodyParser.json());
