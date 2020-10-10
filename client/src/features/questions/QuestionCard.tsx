1f8c84a57e350c98364e3707d892152f5ec10cc2
Markim Shaw <ms79723@gmail.com>
2020-10-09 23:36:00 -0500

    Add Question list
    Add a page to look at all saved questions

---
+++ client/src/features/questions/QuestionCard.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Grid, Paper, makeStyles, Typography } from "@material-ui/core";
+
+export interface MultipleChoiceQuestion {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  id: number;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
+
+type QuestionComponent = {
+  question: MultipleChoiceQuestion;
+};
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    width: 500,
+  },
+}));
+
+const LeftGrid: React.FC<QuestionComponent> = ({ question }) => (
+  <Grid item xs container direction="column" spacing={2}>
+    <Grid item xs>
+      <Typography gutterBottom variant="subtitle1">
+        {question.question}
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        {question.category}
+      </Typography>
+      <Typography variant="body2" color="textSecondary">
+        Author
+      </Typography>
+    </Grid>
+    <Grid item>
+      <Typography variant="body2" style={{ cursor: "pointer", color: "red" }}>
+        Remove
+      </Typography>
+    </Grid>
+  </Grid>
+);
+
+const RightGrid: React.FC<QuestionComponent> = ({ question }) => (
+  <Grid item>
+    <Typography variant="subtitle1">{question.type}</Typography>
+  </Grid>
+);
+
+export const QuestionCard: React.FC<{
+  question: MultipleChoiceQuestion;
+  onPress: (question: MultipleChoiceQuestion) => void;
+}> = ({ question, onPress }) => {
+  const theme = useStyles();
+
+  return (
+    <Paper
+      className={theme.paper}
+      onClick={() => {
+        onPress(question);
+      }}
+    >
+      <Grid item xs={12} sm container>
+        <LeftGrid question={question} />
+        <RightGrid question={question} />
+      </Grid>
+    </Paper>
+  );
+};
---
+++ client/src/features/questions/QuestionsList.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Grid } from "@material-ui/core";
+import { MultipleChoiceQuestion, QuestionCard } from "./QuestionCard";
+
+export const QuestionList = () => {
+  const [questions, setQuestions] = useState<MultipleChoiceQuestion[]>([]);
+  const allQuestions = axios.get("/api/multiple");
+  const [selectedQuestion, setSelectedQuestion] = useState<
+    MultipleChoiceQuestion
+  >();
+
+  useEffect(() => {
+    allQuestions.then((response) => {
+      const decodable: MultipleChoiceQuestion[] = response.data;
+      setQuestions(decodable);
+    });
+  }, []);
+
+  if (selectedQuestion) {
+    return <h1>Preview Questions</h1>;
+  }
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      style={{ padding: 16 }}
+    >
+      {questions.map((question) => (
+        <QuestionCard
+          question={question}
+          onPress={(question) => {
+            console.log("You selected", question.question);
+            setSelectedQuestion(question);
+          }}
+        />
+      ))}
+    </Grid>
+  );
+};
