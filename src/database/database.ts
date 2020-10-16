1c51b0f6100804fc8578a547779ee88331e724cb
Markim Shaw <ms79723@gmail.com>
2020-10-16 01:53:43 -0500

    Perform manual migration

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,14 +1,21 @@
 import * as Realm from "realm";
 
 export class Database {
+  /* 7.  Add uuid to question objects */
   /* 6. Adds name prop */
   /* 5. Adds inReview prop */
-  public static readonly SCHEMA_VERSION = 6;
+  public static readonly SCHEMA_VERSION = 7;
 
   realm(schema): Promise<Realm> {
     return Realm.open({
       schema: [schema],
       schemaVersion: Database.SCHEMA_VERSION,
+      migration: (oldRealm, newRealm) => {
+        if (oldRealm.schemaVersion < 7) {
+          const oldObjects = newRealm.objects("MultipleChoice");
+          newRealm.delete(oldObjects);
+        }
+      },
     });
   }
 }
