75bf16ba574057939c117125041fa2b036dcd463
Markim Shaw <ms79723@gmail.com>
2020-10-16 01:08:43 -0500

    Bump database version

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,8 +1,9 @@
 import * as Realm from "realm";
 
 export class Database {
+  /* 6. Adds name prop */
   /* 5. Adds inReview prop */
-  public static readonly SCHEMA_VERSION = 5;
+  public static readonly SCHEMA_VERSION = 6;
 
   realm(schema): Promise<Realm> {
     return Realm.open({
