bfa3507d284b344ede18eb512ad154b7839f27c0
Eyesofbanquo <djsalinger67@live.com>
2020-10-25 20:10:29 -0500

    Add default values (#53)
    Co-authored-by: Markim Shaw <ms79723@gmail.com>
    
    [Delivers #175430917]

--- src/database/pool.ts
+++ src/database/pool.ts
@@ -1,11 +1,13 @@
 import { Pool } from "pg";
-import { CATEGORY_TABLE } from "../api/category/routes";
+import { v4 as uuidv4 } from "uuid";
 import { createQuestionTable, QUESTION_TABLE } from "../api/question/queries";
 import {
   QUESTION_TYPE_TABLE,
   createQuestionTypeTable,
+  createQuestionType,
 } from "../api/question-type/queries";
 import * as dotenv from "dotenv";
+import { createCategory, CATEGORIES_TABLE } from "../api/category/queries";
 
 const DATABASE = "qizzo";
 
@@ -18,7 +20,7 @@ const createProductionDatabase = async () => {
 const createTables = async () => {
   await pool
     .query(
-      `CREATE TABLE IF NOT EXISTS ${CATEGORY_TABLE}
+      `CREATE TABLE IF NOT EXISTS ${CATEGORIES_TABLE}
   (id UUID PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   inReview BOOLEAN NOT NULL,
@@ -37,6 +39,22 @@ const createTables = async () => {
     .catch((err) => console.log(err));
 };
 
+const createDefaultValues = async () => {
+  await pool
+    .query(createCategory({ table: CATEGORIES_TABLE }), [
+      uuidv4(),
+      "League of Legends",
+      true,
+    ])
+    .catch((err) => console.log(err));
+  await pool
+    .query(createQuestionType({ table: QUESTION_TYPE_TABLE }), [
+      uuidv4(),
+      "pairs",
+    ])
+    .catch();
+};
+
 dotenv.config();
 
 let databaseConfig;
@@ -53,10 +71,12 @@ if (process.env.DATABASE_URL) {
   // create-tables
   createProductionDatabase().catch();
   createTables().catch();
+  createDefaultValues().catch();
 }
 
 if (process.env.LOCAL_DATABASE) {
   createTables().catch();
+  createDefaultValues().catch();
 }
 
 export default pool;
