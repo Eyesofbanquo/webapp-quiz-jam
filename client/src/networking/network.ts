c1bc501216cd9ee6b04e5c805d5dcefc63513242
Markim Shaw <ms79723@gmail.com>
2020-10-30 02:16:54 -0500

    Get contract tests working for one category case

--- client/src/networking/network.ts
+++ client/src/networking/network.ts
@@ -2,13 +2,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface RequestOptions {
+  base?: string;
+  port?: string;
   endpoint: "multiple" | "categories" | "question-types" | "questions";
   method: "get" | "post" | "delete" | "put";
   data?: any;
 }
 
 export const makeRequest = (props: RequestOptions) => {
-  const { endpoint, method, data } = props;
+  const { endpoint, method, data, base, port } = props;
   const uri = `/api/${endpoint}`;
   var options = {
     method: method,
@@ -18,7 +20,26 @@ export const makeRequest = (props: RequestOptions) => {
     options = optionsWithData;
   }
 
+  if (base && port) {
+    var optionsWithBaseAndPort = {
+      ...options,
+      baseURL: `http://${base}:${port}`,
+    };
+    options = optionsWithBaseAndPort;
+  }
+
+  console.log(options);
+
   var request = axios(uri, options);
+  // var request = axios.request({
+  //   url: "/categories",
+  //   baseURL: "http://127.0.0.1:4000",
+  //   method: "GET",
+  //   headers: {
+  //     Accept: "application/json; charset=utf-8",
+  //     "Content-Type": "application/json; charset=utf-8",
+  //   },
+  // });
 
   return {
     onReceive: request,
