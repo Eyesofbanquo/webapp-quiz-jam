e5d3b266cb813e80566c3ee0fc09109c6f2d6332
Markim Shaw <ms79723@gmail.com>
2020-11-06 02:30:03 -0600

    Fix failing tests

--- test/handlers/category-handler.ts
+++ test/handlers/category-handler.ts
@@ -8,26 +8,34 @@ import { v4 as uuidv4 } from "uuid";
 
 export const categoriesExist = async () => {
   await pool.query(createCategoriesTable()).catch();
-  await pool.query(createCategory(), [uuidv4(), "Random", true, false]).catch();
+  await createCategory({
+    id: uuidv4(),
+    name: "Random",
+    in_review: true,
+    deleted: false,
+  }).catch();
   return Promise.resolve("Categories added to database");
 };
 
 export const categoryNightExist = async () => {
   await pool.query(createCategoriesTable()).catch();
-  await pool.query(createCategory(), [uuidv4(), "Night", true, false]).catch();
+  await createCategory({
+    id: uuidv4(),
+    name: "Night",
+    in_review: true,
+    deleted: false,
+  }).catch();
   return Promise.resolve("Category Night added to database");
 };
 
 export const deleteCategoryWithUUID = async () => {
   await pool.query(createCategoriesTable()).catch();
-  await pool
-    .query(createCategory(), [
-      "97dcb062-ffea-4885-baf3-a04ede5b0037",
-      "Ra",
-      true,
-      false,
-    ])
-    .catch();
+  await createCategory({
+    id: "97dcb062-ffea-4885-baf3-a04ede5b0037",
+    name: "Ra",
+    in_review: true,
+    deleted: false,
+  }).catch();
   return Promise.resolve(
     "Category with uuid 97dcb062-ffea-4885-baf3-a04ede5b0037 created"
   );
