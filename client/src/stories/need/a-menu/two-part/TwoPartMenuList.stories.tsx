7db90be9f16e732fea296268a5c242dcaebb6292
Eyesofbanquo <djsalinger67@live.com>
2020-11-08 15:21:18 -0600

    Add storybook + chromatic (#70)
    [Delivers #175632127]

---
+++ client/src/stories/need/a-menu/two-part/TwoPartMenuList.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from "@storybook/react/types-6-0";
+
+import { GenericMenuList } from "../../../../need/a-menu/two-part/GenericMenuList";
+
+export default {
+  title: "Qizzo/Menus",
+  component: GenericMenuList,
+} as Meta;
+
+interface Props {
+  title: string;
+  subtitle: string;
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}
+
+const Template: Story<Props> = (args) => <GenericMenuList {...args} />;
+
+export const Main = Template.bind({});
+Main.args = {
+  title: "Margaux",
+  subtitle: "Pernin",
+  onClick: (event) => {},
+};
