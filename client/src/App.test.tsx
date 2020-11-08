13281faf94adf4864c4a225b9b7a9857c84846b1
Markim Shaw <ms79723@gmail.com>
2020-11-08 01:51:20 -0600

    Test run on e2e style testing

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,35 +1,35 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import puppeteer from "puppeteer";
-import App from "./App";
+// import React from "react";
+// import { render } from "@testing-library/react";
+// import puppeteer from "puppeteer";
+// import App from "./App";
 
-const isDebugging = () => {
-  const debugging_mode = {
-    headless: false,
-    slowMo: 250,
-    devtools: true,
-  };
+// const isDebugging = () => {
+//   const debugging_mode = {
+//     headless: false,
+//     slowMo: 250,
+//     devtools: true,
+//   };
 
-  return process.env.NODE_ENV === "test" ? debugging_mode : {};
-};
+//   return process.env.NODE_ENV === "test" ? debugging_mode : {};
+// };
 
-describe("on page load", () => {
-  test("loads correctly", async () => {
-    let browser = await puppeteer.launch({});
-    let page = await browser.newPage();
+// describe("on page load", () => {
+//   test("loads correctly", async () => {
+//     let browser = await puppeteer.launch({});
+//     let page = await browser.newPage();
 
-    page.emulate({
-      viewport: {
-        width: 500,
-        height: 2400,
-      },
-      userAgent: "",
-    });
+//     page.emulate({
+//       viewport: {
+//         width: 500,
+//         height: 2400,
+//       },
+//       userAgent: "",
+//     });
 
-    await page.goto("http://localhost:3000/");
-    const navbar = await page.$eval(".nav", (e) => e.className);
-    console.log(navbar);
-    expect(navbar).toBeTruthy();
-    browser.close();
-  });
-});
+//     await page.goto("http://localhost:3000/");
+//     const navbar = await page.$eval(".nav", (e) => e.className);
+//     console.log(navbar);
+//     expect(navbar).toBeTruthy();
+//     browser.close();
+//   });
+// });
--- src/tests/random.ts
+++ src/tests/random.ts
@@ -1,75 +1,75 @@
-// import puppeteer from "puppeteer";
-// import "puppeteer";
-import * as puppeteer from "puppeteer";
-import { AppController } from "../../server";
-import * as chai from "chai";
-import { expect } from "chai";
-import "mocha";
-import "chai-http";
-chai.use(require("chai-http"));
+// // import puppeteer from "puppeteer";
+// // import "puppeteer";
+// import * as puppeteer from "puppeteer";
+// import { AppController } from "../../server";
+// import * as chai from "chai";
+// import { expect } from "chai";
+// import "mocha";
+// import "chai-http";
+// chai.use(require("chai-http"));
 
-const isDebugging = () => {
-  const debugging_mode = {
-    headless: false,
-    slowMo: 250,
-    devtools: true,
-  };
+// const isDebugging = () => {
+//   const debugging_mode = {
+//     headless: false,
+//     slowMo: 250,
+//     devtools: true,
+//   };
 
-  return process.env.NODE_ENV === "test" ? debugging_mode : {};
-};
+//   return process.env.NODE_ENV === "test" ? debugging_mode : {};
+// };
 
-describe("on page load", async () => {
-  it("loads correctly", (done) => {
-    const controller = new AppController();
-    chai
-      .request(controller.app)
-      .get("/")
-      .then(async (result) => {
-        let browser = await puppeteer.launch(isDebugging());
-        let page = await browser.newPage();
+// describe("on page load", async () => {
+//   it("loads correctly", (done) => {
+//     const controller = new AppController();
+//     chai
+//       .request(controller.app)
+//       .get("/")
+//       .then(async (result) => {
+//         let browser = await puppeteer.launch(isDebugging());
+//         let page = await browser.newPage();
 
-        page.emulate({
-          viewport: {
-            width: 500,
-            height: 2400,
-          },
-          userAgent: "",
-        });
+//         page.emulate({
+//           viewport: {
+//             width: 500,
+//             height: 2400,
+//           },
+//           userAgent: "",
+//         });
 
-        await page.goto("http://localhost:5000/");
-        const navbar = await page.$eval("#root", (e) => e.id);
-        console.log(navbar);
-        expect(navbar).to.not.equal(undefined);
-        browser.close();
-        done();
-      })
-      .catch();
-  });
+//         await page.goto("http://localhost:5000/");
+//         const navbar = await page.$eval("#root", (e) => e.id);
+//         console.log(navbar);
+//         expect(navbar).to.not.equal(undefined);
+//         browser.close();
+//         done();
+//       })
+//       .catch();
+//   });
 
-  it("loads correctly", (done) => {
-    const controller = new AppController();
-    chai
-      .request(controller.app)
-      .get("/")
-      .then(async (result) => {
-        let browser = await puppeteer.launch(isDebugging());
-        let page = await browser.newPage();
+//   it("loads correctly", (done) => {
+//     const controller = new AppController();
+//     chai
+//       .request(controller.app)
+//       .get("/")
+//       .then(async (result) => {
+//         let browser = await puppeteer.launch(isDebugging());
+//         let page = await browser.newPage();
 
-        page.emulate({
-          viewport: {
-            width: 500,
-            height: 2400,
-          },
-          userAgent: "",
-        });
+//         page.emulate({
+//           viewport: {
+//             width: 500,
+//             height: 2400,
+//           },
+//           userAgent: "",
+//         });
 
-        await page.goto("http://localhost:5000/");
-        const navbar = await page.$eval("#root", (e) => e.id);
-        console.log(navbar);
-        expect(navbar).to.not.equal(undefined);
-        browser.close();
-        done();
-      })
-      .catch();
-  });
-});
+//         await page.goto("http://localhost:5000/");
+//         const navbar = await page.$eval("#root", (e) => e.id);
+//         console.log(navbar);
+//         expect(navbar).to.not.equal(undefined);
+//         browser.close();
+//         done();
+//       })
+//       .catch();
+//   });
+// });
