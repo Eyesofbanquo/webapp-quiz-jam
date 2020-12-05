e390d77082a3971bc83a21dcdd21a2c20a4e6e12
Markim Shaw <ms79723@gmail.com>
2020-12-05 17:39:54 -0600

    Refactor makeRequest to accept auth endpoints

--- client/src/networking/network.ts
+++ client/src/networking/network.ts
@@ -1,22 +1,54 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+type QuizServiceEndpoints =
+  | "multiple"
+  | "categories"
+  | "question-types"
+  | "questions"
+  | "difficulty";
+
+type AuthServiceEndpoints = "users" | "login" | "token" | "logout";
+
+type Microservice = "quiz" | "auth";
+
+const baseUri = (service: Microservice) => {
+  let uri;
+  if (service === "quiz") {
+    uri = `/api`;
+  }
+
+  if (service === "auth") {
+    uri = process.env.REACT_APP_AUTH_SERVICE_URI ?? `/error`;
+  }
+
+  if (uri === undefined) {
+    return "/error";
+  }
+
+  return uri;
+};
+
 interface RequestOptions {
   base?: string;
   port?: string;
-  endpoint:
-    | "multiple"
-    | "categories"
-    | "question-types"
-    | "questions"
-    | "difficulty";
+  service?: "quiz" | "auth";
+  endpoint: QuizServiceEndpoints | AuthServiceEndpoints;
   method: "get" | "post" | "delete" | "put";
   data?: any;
 }
 
-export const makeRequest = (props: RequestOptions) => {
-  const { endpoint, method, data, base, port } = props;
-  let uri = `/api/${endpoint}`;
+export const makeRequest = ({
+  endpoint,
+  method,
+  data,
+  base,
+  port,
+  service = "quiz",
+}: RequestOptions) => {
+  let serviceBaseDomain = baseUri(service);
+  let uri = `${serviceBaseDomain}/${endpoint}`;
+
   var options = {
     method: method,
   };
@@ -58,15 +90,22 @@ export const makeRequest = (props: RequestOptions) => {
     onReceive: request,
   };
 };
-export function useMakeRequest<T>(props: RequestOptions) {
+export function useMakeRequest<T>({
+  endpoint,
+  method,
+  data,
+  base,
+  port,
+  service = "quiz",
+}: RequestOptions) {
   const [request, setRequest] = useState<T>();
   useEffect(() => {
     makeRequest({
-      endpoint: props.endpoint,
-      method: props.method,
-      data: props.data,
+      endpoint: endpoint,
+      method: method,
+      data: data,
+      service: service,
     }).onReceive.then((result) => {
-      console.log("from-network-call", result);
       setRequest(result.data);
     });
   }, []);
