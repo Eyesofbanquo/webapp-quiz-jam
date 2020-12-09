e2f24f2a727faa429ecbe154cdc6186baf8c7a3f
Markim Shaw <ms79723@gmail.com>
2020-12-09 00:13:34 -0600

    Refactor LoginScreen into a component

--- client/src/screens/login/LoginScreen.tsx
+++ client/src/screens/login/LoginScreen.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Grid, Typography, Button } from "@material-ui/core";
 
-export const LoginScreen = () => (
-  <div>
-    <h1>Login Screen</h1>
-  </div>
+const LoginForm = () => (
+  <Container maxWidth="lg">
+    <div>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <div id={"login-description"}>
+          <Typography variant="h3">
+            Welcome to the admin page for the <b>Quizzo</b> builder
+          </Typography>
+        </div>
+        <Button
+          style={{ marginTop: 16 }}
+          id={"login-screen-signup-button"}
+          variant="contained"
+          color="primary"
+          onClick={(event) => {
+            console.log(event, "Login");
+          }}
+        >
+          Login
+        </Button>
+        <Button
+          style={{ marginTop: 16 }}
+          id={"login-screen-login-button"}
+          variant="contained"
+          color="primary"
+          onClick={(event) => {
+            console.log(event, "Sign up");
+          }}
+        >
+          Sign up
+        </Button>
+      </Grid>
+    </div>
+  </Container>
 );
+
+export const LoginScreen = () => {
+  return <LoginForm />;
+};
